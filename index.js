be4dc7a094cdb14b6abdd470e63c66e0f0444334
Subhranil Ghoshal <ghoshalsubhranil@gmail.com>
2024-01-06 12:01:13 +0530

    Initial implementation of night mode

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 function menu(){
 	let elem = document.getElementById("sidebar");
 	let btn = document.getElementById("btn");
-	if (elem.style.left=="-20%"){
+	let overlay = document.getElementById("overlay");
+	let night = document.getElementsByTagName("body")[0].style.backgroundColor == "rgb(32, 32, 32)";
+	if (elem.style.left=="-20vw"){
 		elem.style.left="0px";
 		btn.style.color = "white";
+		overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
 	}
 	else{
-		elem.style.left = "-20%";
-		btn.style.color = "black";
+		elem.style.left = "-20vw";
+		if(!night){
+			btn.style.color = "black";
+		}
+		overlay.style.backgroundColor = "rgba(0,0,0,0)";
+	}
+}
+
+function togglenight(){
+	let btn = document.getElementById("btn");
+	let body = document.getElementsByTagName("body")[0];
+	if(body.style.backgroundColor=="rgb(32, 32, 32)"){
+		body.style.backgroundColor="white";
+		body.style.color = "black";
+	}
+	else{
+		body.style.backgroundColor="rgb(32, 32, 32)";
+		body.style.color="white";
 	}
 }
